231840f671444434f8a4849dead06c9cc79e2c37
amy <ctrlalternativedelete@gmail.com>
2025-08-01 10:05:14 +1000

    fix: linux support

--- run.sh
+++ run.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-bazel test --cxxopt=/std:c++20 --test_output=all //:main
+PROGRAM="//:main"
+if [[ "$OSTYPE" == "win32" ]]; then
+    bazel test --cxxopt=/std:c++20 --test_output=all $PROGRAM
+else
+    bazel test --cxxopt=-std=c++20 --test_output=all $PROGRAM
+fi
